8a705a966da45920bb15f0f4f4c04950ad8637ca
zhangxigithub <zhangxi_1989@sina.com>
2013-11-19 02:44:42 +0800

    提交 1.0

--- Pods/Pods-GGFullscreenImageViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GGFullscreenImageViewController : NSObject
-@end
-@implementation PodsDummy_Pods_GGFullscreenImageViewController
-@end
--- Pods/Pods-KLSwitch-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KLSwitch : NSObject
-@end
-@implementation PodsDummy_Pods_KLSwitch
-@end
